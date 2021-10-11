66504204773c4c72b6ed458d0c9595789cd4c09b
Martin Johansson <marjoh3@live.se>
2021-10-11 14:51:11 +0200

    Empty reposistory

---
+++ Assignment4_ToDoIT_MJ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment4_ToDoIT_MJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
