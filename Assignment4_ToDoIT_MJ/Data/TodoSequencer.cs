e84f74a259d8cdb18f82783beda0e04afd42ef23
Martin Johansson <marjoh3@live.se>
2021-10-13 08:58:09 +0200

    adding Todosequencer and unit test

---
+++ Assignment4_ToDoIT_MJ/Data/TodoSequencer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4_ToDoIT_MJ.Data
+{
+    public class TodoSequencer
+    {
+        private static int toDoId;
+        public static int getToDoId() { return toDoId; }
+        public static int nextToDoId() { toDoId++; return toDoId; }
+        public static void reset() { toDoId = 0; }
+    }
+}
---
+++ ToDoIT_XUNIT/ToDoSequencerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using Assignment4_ToDoIT_MJ.Data;
+
+namespace ToDoIT_XUNIT
+{
+    public class ToDoSequencerTests
+    {
+
+        [Fact]
+        public void ResetTodoSequencerTest() //this should now be 0
+        {
+            TodoSequencer.reset();
+            Assert.Equal(0, TodoSequencer.getToDoId());
+        }
+        [Fact]
+        public void IncrementTodoSequencerTest() //test if personId is incremented
+        {
+            int previous = TodoSequencer.getToDoId();
+            Assert.Equal((previous + 1), TodoSequencer.nextToDoId());
+        }
+        [Fact]
+        public void TodoSequencerIs1() //PersonId should be 1 because I incremented once
+        {
+            Assert.Equal(1, TodoSequencer.getToDoId());
+        }
+    }
+}
