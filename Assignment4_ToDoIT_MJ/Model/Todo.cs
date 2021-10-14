33f6d0708ab3c970178284f27dc3e4ccca2e3af2
Martin Johansson <marjoh3@live.se>
2021-10-14 10:37:04 +0200

    missed stage 4 with adding todo so I am doing it now

---
+++ Assignment4_ToDoIT_MJ/Model/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4_ToDoIT_MJ.Model
+{
+    public class Todo
+    {
+        private string description;
+        private readonly int todoId;
+        private bool done;
+        private Person assignee;
+        public Todo(int todoId,string description) //at this stage only todoID and desctption should be set.
+        {
+            this.todoId = todoId;
+            this.Description = description;
+            
+        }
+        public int GetTodoID() { return todoId; }
+        public String Description { get => description; set => description=value; } //there were no rules set at this stage
+    }
+}
---
+++ ToDoIT_XUNIT/todoTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Assignment4_ToDoIT_MJ.Model;
+
+namespace ToDoIT_XUNIT
+{
+    public class todoTests
+    {
+        
+        private Todo testTodo = new Todo(1, "Read chapter 1");
+        [Fact]
+        public void CheckDescriptionAndIDvalues() 
+        {
+            Assert.True(testTodo.GetTodoID() == 1 && testTodo.Description == "Read chapter 1");
+        }
+    }
+}
